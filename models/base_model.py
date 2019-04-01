75bf58d4550885c99f005bc0e3db248aeee78b16
stefansilverio <494@holbertonschool.com>
2019-04-01 23:34:26 +0000

    delete _sa_instance_state

--- models/base_model.py
+++ models/base_model.py
@@ -68,6 +68,7 @@ class BaseModel:
         my_dict["__class__"] = str(type(self).__name__)
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
+        del my_dict["_sa_instance_state"]
         return my_dict
 
     def delete(self):
