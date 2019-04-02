4c5b3aa87b2233ad914df9ed340da051d96c2172
stefansilverio <494@holbertonschool.com>
2019-04-02 20:01:32 +0000

    import Base and column headers

--- models/city.py
+++ models/city.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """This is the city class"""
 from models.base_model import BaseModel
-
+from models.base_model import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 class City(BaseModel, Base):
     """This is the class for City
@@ -9,6 +10,6 @@ class City(BaseModel, Base):
         state_id: The state id
         name: input name
     """
-    __tablename__ = cities
+    __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), nullable=False, ForeignKey('states.id')
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
