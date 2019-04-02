fffd6ba5340ebee8f01c6cffbe60f1630d451447
stefansilverio <494@holbertonschool.com>
2019-04-02 19:49:33 +0000

    updated cascade

--- models/state.py
+++ models/state.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """This is the state class"""
 from models.base_model import BaseModel
-
+from models.base_model import Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
 
 class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    __tablename__ = states
-    name = Column(string(128), nullable=False)
-    cities = relationship(
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City", backref="state", cascade="all, delete, delete-orphans")
