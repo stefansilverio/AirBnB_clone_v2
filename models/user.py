d491f6440a6a91b994027b687106fcba0701660b
Athena Deng <539@holbertonschool.com>
2019-04-03 03:56:23 +0000

    added relationship

--- models/user.py
+++ models/user.py
@@ -13,8 +13,10 @@ class User(BaseModel, Base):
         first_name: first name
         last_name: last name
     """
-    __tablename__ = 'user'
+    __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    places = relationship("Place", backref='user', cascade="all, delete, delete-orphan")
