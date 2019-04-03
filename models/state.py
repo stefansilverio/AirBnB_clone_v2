2df27ddb9ac692259c7c7dc540b22aa9eff90b4f
stefansilverio <494@holbertonschool.com>
2019-04-03 19:24:05 +0000

    add getter method

--- models/state.py
+++ models/state.py
@@ -12,8 +12,9 @@ class State(BaseModel, Base):
     """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+
     cities = relationship("City", backref='state', cascade="all, delete, delete-orphan")
-    
+
     @property
     def cities(self):
         """ gets a list of city objects with that state id """
