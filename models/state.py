97d4f977eb82ea1cf3b40347e11203ea34deb236
Athena Deng <539@holbertonschool.com>
2019-04-04 01:59:29 +0000

    fixed pep8

--- models/state.py
+++ models/state.py
@@ -5,6 +5,7 @@ from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+
 class State(BaseModel, Base):
     """This is the class for State
     Attributes:
@@ -13,7 +14,8 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
 
-    cities = relationship("City", backref='state', cascade="all, delete, delete-orphan")
+    cities = relationship(
+            "City", backref='state', cascade="all, delete, delete-orphan")
 
     @property
     def cities(self):
