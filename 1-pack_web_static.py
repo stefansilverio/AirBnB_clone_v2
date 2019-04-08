2a86e29cde9d6d973378d310d31e65b1ace88b19
stefansilverio <494@holbertonschool.com>
2019-04-08 23:47:49 +0000

    compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""compress files"""
+from datetime import datetime
+from fabric.api import local
+import os
+
+
+def do_pack():
+    """create tarball"""
+    now = datetime.now()
+    local("mkdir -p versions")
+    name = "web_static_" + str(now.year) + str(now.month) + str(now.day)\
+           + str(now.hour) + str(now.minute) +  str(now.second) + ".tgz"
+    local("tar czfv {} web_static".format(name))
+    local("mv {} versions".format(name))
+    exists = os.path.isfile('versions/{}'.format(name))
+    if (exists):
+        return(os.path.abspath("{}".format(name)))
+    return None
