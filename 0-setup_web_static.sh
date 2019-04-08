58e6e805fc35856fb151700985dd2122327aa2f6
stefansilverio <494@holbertonschool.com>
2019-04-08 20:01:22 +0000

    configure web_server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,7 +7,7 @@ sudo touch /data/web_static/releases/test/index.html
 echo 'butter' > /data/web_static/releases/test/index.html
 sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo echo "server {
+echo "server {
     listen 80 default_server;
     location / {
              add_header X-Served-By 494-web-01;
