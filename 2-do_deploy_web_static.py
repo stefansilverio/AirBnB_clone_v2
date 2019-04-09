2bbae5c56428c1e02fa09af33735e8ddc3575e01
stefansilverio <494@holbertonschool.com>
2019-04-09 03:30:59 +0000

    deploy archives

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,23 +11,28 @@ env.hosts = ['35.237.253.193', '35.231.141.38']
 
 
 def do_deploy(archive_path):
+    a_list = archive_path.split("/")
+    filename = a_list[-1]
+    extension_list = filename.split(".")
+    no_extension = extension_list[0]
     exists = os.path.isfile('{}'.format(archive_path))
     if not exists:
         return False
     try:
-        put("versions/web_static_2019490105.tgz", "/tmp/")
-        run("rm -rf /data/web_static/releases/web_static_2019490105/")
-        run("mkdir -p /data/web_static/releases/web_static_2019490105")
-        run("tar xzf /tmp/web_static_2019490105.tgz -C \
-/data/web_static/releases/web_static_2019490105")
-        run("mv /data/web_static/releases/web_static_2019490105/web_static/* \
-        /data/web_static/releases/web_static_2019490105/")
-        sudo("rm -f /tmp/web_static_2019490105.tgz")
-        sudo("rm -rf /data/web_static/releases/web_static_2019490105/ \
-web_static/")
+        put(archive_path, "/tmp/")
+        run("rm -rf /data/web_static/releases/" + no_extension)
+        run("mkdir -p /data/web_static/releases/" + no_extension)
+        run("tar xzf /tmp/" + filename + " -C " +
+            "/data/web_static/releases/" + no_extension)
+        run("mv /data/web_static/releases/" +
+            no_extension + "/web_static/* /data/web_static/releases/" +
+            no_extension)
+        sudo("rm -f /tmp/" + filename)
+        sudo("rm -rf /data/web_static/releases/" +
+             no_extension + "web_static/")
         sudo("rm -rf /data/web_static/current")
-        sudo("ln -s /data/web_static/releases/web_static_2019490105 \
-/data/web_static/current")
+        sudo("ln -s /data/web_static/releases/" + no_extension +
+             " /data/web_static/current")
         return True
-    except Exception as e:
+    except Exception:
         return False
