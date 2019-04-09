1355f2bf8014e93930efada1477395cbb81b0147
stefansilverio <494@holbertonschool.com>
2019-04-09 04:11:11 +0000

    distribute archives to web-servers

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""distribute archive to web-servers"""
+from datetime import datetime
+from fabric.api import put
+from fabric.api import sudo
+from fabric.api import hosts, env
+from fabric.api import local
+import os
+
+
+env.hosts = ['35.237.253.193', '35.231.141.38']
+
+
+def do_pack():
+    """create tarball"""
+    now = datetime.now()
+    local("mkdir -p versions")
+    name = "web_static_" + str(now.year) + str(now.month) + str(now.day)\
+           + str(now.hour) + str(now.minute) + str(now.second) + ".tgz"
+    local("tar czfv {} web_static".format(name))
+    local("mv {} versions".format(name))
+    exists = os.path.isfile('versions/{}'.format(name))
+    if (exists):
+        return(os.path.abspath("{}".format(name)))
+    return None
+
+
+def do_deploy(archive_path):
+    a_list = archive_path.split("/")
+    filename = a_list[-1]
+    extension_list = filename.split(".")
+    no_extension = extension_list[0]
+    exists = os.path.isfile('{}'.format(archive_path))
+    if not exists:
+        return False
+    try:
+        put(archive_path, "/tmp/")
+        run("rm -rf /data/web_static/releases/" + no_extension)
+        run("mkdir -p /data/web_static/releases/" + no_extension)
+        run("tar xzf /tmp/" + filename + " -C " +
+            "/data/web_static/releases/" + no_extension)
+        run("mv /data/web_static/releases/" +
+            no_extension + "/web_static/* /data/web_static/releases/" +
+            no_extension)
+        sudo("rm -f /tmp/" + filename)
+        sudo("rm -rf /data/web_static/releases/" +
+             no_extension + "web_static/")
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -s /data/web_static/releases/" + no_extension +
+             " /data/web_static/current")
+        return True
+    except Exception:
+        return False
+
+
+def deploy():
+    path = do_pack()
+    if path is None:
+        return False
+    return (do_deploy(path))
