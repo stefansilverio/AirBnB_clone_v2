f938869522d098ea9e620bcae31c406bb460fadd
stefansilverio <494@holbertonschool.com>
2019-04-10 04:11:32 +0000

    clean old archives

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -4,13 +4,16 @@ from fabric.api import env, hosts, local, run
 from fabric.context_managers import lcd, cd
 
 
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/holberton'
 env.hosts = ['35.237.253.193', '35.231.141.38']
 
 
 def do_clean(number=0):
+    """clean old archives"""
     if number is 0:
         number = 1
     with lcd("versions/"):
-        local("ls -t | tail -n+{} | xargs rm".format(number))
+        local("ls -Ct | tail -n+{} | xargs rm -f".format(number))
     with cd("/data/web_static/releases"):
-        run("ls -t | tail -n+{} | xargs rm".format(number))
+        run("ls -Ct | tail -n+{} | xargs rm -f".format(number))
