de75caa8e6fbaee2d961b96a9e970f53a922cd32
stefansilverio <494@holbertonschool.com>
2019-04-11 05:28:43 +0000

    clean local and remote

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -11,9 +11,10 @@ env.hosts = ['35.237.253.193', '35.231.141.38']
 
 def do_clean(number=0):
     """clean old archives"""
-    if number is 0 or 1:
-        number = 2
+    if int(number) is 0 or int(number) is 1:
+        number = 1
+    number = int(number)
     with lcd("versions/"):
-        local("ls -1t | tail -n+{} | xargs rm -rf".format(number+1))
+        local("ls -1t | tail -n+{} | xargs rm -f".format(number+1))
     with cd("/data/web_static/releases"):
         run("ls -1t | tail -n+{} | xargs rm -rf".format(number+1))
