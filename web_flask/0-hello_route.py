8d87386cde6b8a1c30f21e841960aa1ca33c6671
stefansilverio <494@holbertonschool.com>
2019-04-22 23:17:30 +0000

    run basic flask app

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""start a flask web-app"""
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    """return text"""
+    return "Hello HBNB!"
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=5000)
