bac070bee590e5d46faaef1ce699015cacc82fa3
stefansilverio <494@holbertonschool.com>
2019-04-24 21:05:37 +0000

    app teardown above route

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,16 +7,17 @@ app = Flask(__name__)
 all_states = storage.all(State)
 
 
+@app.teardown_appcontext
+def new_session(error):
+    """end current session"""
+    storage.close()
+
+
 @app.route('/states_list', strict_slashes=False)
 def s():
     """show all states"""
     return render_template('7-states_list.html', state=all_states.values())
 
-
-@app.teardown_appcontext
-def new_session(error):
-    """end current session"""
-    storage.close()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
