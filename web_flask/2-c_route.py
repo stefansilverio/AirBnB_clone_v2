93a878b914add262428fb3efe7d6b1a53b3b43ef
stefansilverio <494@holbertonschool.com>
2019-04-24 16:18:03 +0000

    added spaces

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -19,7 +19,8 @@ def hello():
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     """handle variable"""
-    return "C %s" % text
+    text = text.replace("_", " ")
+    return ("C {}".format(text))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
