f6812b568391925d46815dcb513ae8a65dc40292
stefansilverio <494@holbertonschool.com>
2019-04-24 23:55:44 +0000

    test checker

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-"""list all states"""
-from models import storage
-from models.state import State
+"""Python script starts Flask web application for requests"""
 from flask import Flask, render_template
+from models import storage
+
 
 app = Flask(__name__)
-all_states = storage.all(State)
 
 
 @app.teardown_appcontext
-def new_session(error):
-    """end current session"""
+def tearDown(error):
+    """Tear down process when app stops running"""
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def s():
-    """show all states"""
-    return render_template('7-states_list.html', state=all_states.values())
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Lists states in database"""
+    return render_template("7-states_list.html",
+                           state_list=storage.all("State"))
 
 
 if __name__ == "__main__":
     storage.close()
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
