774d47378b733675692f5a632df58d70e4bc13e2
stefansilverio <494@holbertonschool.com>
2019-04-24 20:41:02 +0000

    show me all the states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""list all states"""
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+app = Flask(__name__)
+all_states = storage.all(State)
+
+
+@app.route('/states_list', strict_slashes=False)
+def s():
+    """show all states"""
+    return render_template('7-states_list.html', state=all_states.values())
+
+
+@app.teardown_appcontext
+def new_session(all_states):
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
