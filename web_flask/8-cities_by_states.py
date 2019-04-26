8d015b409728a2ff2c101ee9c2155a8735930ab6
stefansilverio <494@holbertonschool.com>
2019-04-26 19:41:22 +0000

    print cities by state

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -8,15 +8,18 @@ from flask import Flask, render_template
 app = Flask(__name__)
 all_states = storage.all(State)
 
+
 @app.teardown_appcontext
 def new_session(error):
     """remove current session"""
     storage.close()
 
+
 @app.route('/cities_by_states', strict_slashes=False)
 def c():
     """send obj to jinja template"""
-    return render_template('8-cities_by_states.html', states=all_states.values())
+    return render_template('8-cities_by_states.html',
+                           states=all_states.values())
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
